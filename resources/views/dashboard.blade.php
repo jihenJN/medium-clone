9864ae277262c81e4a0d80a233a15289718179d0
jihenJN <jihenjihen23@gmail.com>
2025-12-22 12:12:35 +0100

    feat:add flowbite and test it

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,24 +5,24 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <ul>
                         @foreach ($categories as $category)
-                             <li>{{ $category->name }}</li>
+                        <li>{{ $category->name }}</li>
                         @endforeach
                     </ul>
 
                 </div>
             </div>
-             <div class ="mt-4"></div>
-             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="mt-4"></div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <ul>
                         @foreach ($posts as $post)
-                             <li>{{ $post->title }}</li>
+                        <li>{{ $post->title }}</li>
                         @endforeach
                     </ul>
 
@@ -30,5 +30,26 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                    <button data-modal-target="testModal" data-modal-toggle="testModal"
+                        class="px-4 py-2 bg-blue-600 text-white rounded">
+                        Open Modal
+                    </button>
+
+                    <div id="testModal" tabindex="-1" class="hidden fixed inset-0 z-50">
+                        <div class="bg-white p-6 rounded shadow">
+                            <p>Hello Flowbite!</p>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
 </x-app-layout>
