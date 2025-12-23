772c72f1d46744da53e36318b4ab353305df42f6
jihenJN <jihenjihen23@gmail.com>
2025-12-23 11:36:37 +0100

    feat:styling categories

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,55 +1,27 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <ul>
-                        @foreach ($categories as $category)
-                        <li>{{ $category->name }}</li>
-                        @endforeach
-                    </ul>
-
-                </div>
-            </div>
-            <div class="mt-4"></div>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <ul>
-                        @foreach ($posts as $post)
-                        <li>{{ $post->title }}</li>
-                        @endforeach
-                    </ul>
-
-                    {{$posts->links()}}
-                </div>
+<div class="py-6">
+    <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-4 text-gray-900">
+                <ul class="flex justify-center flex-wrap text-sm font-medium text-center text-body">
+                    <li class="me-2">
+                        <a href="#" class="inline-block px-4 py-2.5 text-white bg-brand rounded-base active"
+                            aria-current="page">All</a>
+                    </li>
+                    @foreach ($categories as $category)
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">{{$category->name}}</a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
-    </div> --}}
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-
-                    <button data-modal-target="testModal" data-modal-toggle="testModal"
-                        class="px-4 py-2 bg-blue-600 text-white rounded">
-                        Open Modal
-                    </button>
-
-                    <div id="testModal" tabindex="-1" class="hidden fixed inset-0 z-50">
-                        <div class="bg-white p-6 rounded shadow">
-                            <p>Hello Flowbite!</p>
-                        </div>
-                    </div>
+    </div>
+</div>
 
 
-                </div>
-            </div>
-        </div>
+
+
 </x-app-layout>
